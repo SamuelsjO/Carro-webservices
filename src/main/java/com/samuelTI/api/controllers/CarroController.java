7ab3ebd550e277ad2b4c0b9ad9b0e90c3590f58b
Samuel Oliveira <soliveira@cpqd.com.br>
2020-10-14 20:12:04 -0300

    Commit melhorias DELETE, POST, ainda com problemas no metodo PUT

--- src/main/java/com/samuelTI/api/controllers/CarroController.java
+++ src/main/java/com/samuelTI/api/controllers/CarroController.java
@@ -38,9 +38,10 @@ public class CarroController {
 
 		return carro.map(c -> ResponseEntity.ok(c)).orElse(ResponseEntity.notFound().build());
 
-		/* outros metodos de fazer o get
-		 * if(carro.isPresent()) { return ResponseEntity.ok(carro.get()); } else {
-		 * return ResponseEntity.notFound().build(); }
+		/*
+		 * outros metodos de fazer o get if(carro.isPresent()) { return
+		 * ResponseEntity.ok(carro.get()); } else { return
+		 * ResponseEntity.notFound().build(); }
 		 * 
 		 * return carro.isPresent() ? ResponseEntity.ok(carro.get()) :
 		 * ResponseEntity.notFound().build();
@@ -51,48 +52,41 @@ public class CarroController {
 	@GetMapping("/tipo/{tipo}")
 	public ResponseEntity<?> getCarrosByTipo(@PathVariable("tipo") String tipo) {
 		List<CarroDTO> carros = service.getCarrosByTipo(tipo);
-		
-		return carros.isEmpty() ?
-				ResponseEntity.noContent().build() :
-					ResponseEntity.ok(carros);
+
+		return carros.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(carros);
 	}
 
 	@PostMapping
 	public ResponseEntity<?> post(@RequestBody Carro carro) {
-		
+
 		try {
 			CarroDTO c = service.insert(carro);
 			URI location = getUri(c.getId());
 			return ResponseEntity.created(location).build();
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
-	    }
-		
+		}
+
 	}
-	
+
 	private URI getUri(Long id) {
-		return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(id).toUri();
+		return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> put(@PathVariable("id") Long id, @RequestBody Carro carro) {
-		
+
 		carro.setId(id);
-		
 		CarroDTO c = service.update(carro, id);
 
-		return c != null ?
-				ResponseEntity.ok(c) :
-					ResponseEntity.notFound().build();
-
+		return c != null ? ResponseEntity.ok(c) : ResponseEntity.notFound().build();
 	}
 
 	@DeleteMapping("/{id}")
-	public String delete(@PathVariable("id") Long id) {
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 
-		service.delete(id);
+		boolean ok = service.delete(id);
 
-		return "Carro deletado com sucesso!!";
+		return ok ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
 	}
 }
--- src/main/java/com/samuelTI/api/domain/CarroService.java
+++ src/main/java/com/samuelTI/api/domain/CarroService.java
@@ -16,65 +16,66 @@ public class CarroService {
 
 	@Autowired
 	private CarroRepository repository;
-	
-	
-	public List<CarroDTO> getCarros(){
-		
-		return repository.findAll().stream().map(c -> CarroDTO.create(c)).collect(Collectors.toList());
+
+	public List<CarroDTO> getCarros() {
+
+		return repository.findAll().stream().map(CarroDTO::create).collect(Collectors.toList());
 		/*
-		List<Carro> carros = repository.findAll();
-		List<CarroDTO> listDTO = carros.stream().map(c -> new CarroDTO(c)).collect(Collectors.toList());
-		return listDTO*/
-		
-		
+		 * List<Carro> carros = repository.findAll(); List<CarroDTO> listDTO =
+		 * carros.stream().map(c -> new CarroDTO(c)).collect(Collectors.toList());
+		 * return listDTO
+		 */
+
 	}
-	
+
 	public Optional<CarroDTO> getCarrosById(Long id) {
-		return repository.findById(id).map(c -> CarroDTO.create(c));
+		return repository.findById(id).map(CarroDTO::create);
 	}
-	
+
 	public List<CarroDTO> getCarrosByTipo(String tipo) {
-		
-		return repository.findByTipo(tipo).stream().map(c -> CarroDTO.create(c)).collect(Collectors.toList());
+
+		return repository.findByTipo(tipo).stream().map(CarroDTO::create).collect(Collectors.toList());
 	}
-	
-	public CarroDTO insert(Carro carro) { 
-		Assert.isNull(carro.getId() , "Não foi possivel inserir o registro");
-		return CarroDTO.create(repository.save(carro)); 
+
+	public CarroDTO insert(Carro carro) {
+		Assert.isNull(carro.getId(), "Não foi possivel inserir o registro");
+		return CarroDTO.create(repository.save(carro));
 	}
-	
+
 	public CarroDTO update(Carro carro, Long id) {
-		Assert.isNull(carro.getId() , "Não foi possivel atualizar o registro");
-		
-		//Busca o carro no banco de dados
+		Assert.isNull(id, "Não foi possivel atualizar o registro");
+
+		// Busca o carro no banco de dados
 		Optional<Carro> optional = repository.findById(id);
-		if(optional.isPresent()){
-			Carro  db = optional.get();
-			//Copiar as propriedades
+		if (optional.isPresent()) {
+			Carro db = optional.get();
+			// Copiar as propriedades
 			db.setNome(carro.getNome());
 			db.setTipo(carro.getTipo());
 			System.out.println("Carro id " + db.getId());
-			//atualiza o carro
+
+			// atualiza o carro
 			repository.save(db);
-			
+
 			return CarroDTO.create(db);
 		} else {
-			throw new RuntimeException("Não foi possivel atualizar o registro");
+			return null;
+			// throw new RuntimeException("Não foi possivel atualizar o registro");
 		}
-		
+
 	}
-	
-	public void delete(Long id) {
-		Optional<CarroDTO> carro = getCarrosById(id);
-		if(carro.isPresent()) {
+
+	public boolean delete(Long id) {
+		if (getCarrosById(id).isPresent()) {
 			repository.deleteById(id);
+			return true;
 		}
-		
+		return false;
 	}
-	
-	public List<Carro> getCarrosFakes(){
-		List<Carro> carros =  new ArrayList<>();
-		
+
+	public List<Carro> getCarrosFakes() {
+		List<Carro> carros = new ArrayList<>();
+
 		carros.add(new Carro(1L, "Fusca"));
 		carros.add(new Carro(1L, "Brasilia"));
 		carros.add(new Carro(1L, "Corovete"));
