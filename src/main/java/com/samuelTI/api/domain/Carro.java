ff495d10b483e3d894d04eb01ee4313d47acbaca
Samuel Oliveira <soliveira@cpqd.com.br>
2020-10-17 17:20:06 -0300

    Implementado classe de teste, banco H2 e documentaçao da API em Swagger

--- src/main/java/com/samuelTI/api/domain/Carro.java
+++ src/main/java/com/samuelTI/api/domain/Carro.java
@@ -14,16 +14,29 @@ public class Carro {
 	private Long id;
 	private String nome;
 	private String tipo;
+	private String descricao;
+	private String urlFoto;
+	private String urlVideo;
+	private String latitude;
+	private String longitude;
 	
 	public Carro() {
 		
 	}
 	
-	public Carro(Long id, String nome, String tipo) {
+	public Carro(Long id, String nome, String tipo, String descricao, String urlFoto, String urlVideo, String latitude,
+			String longitude) {
+		super();
 		this.id = id;
 		this.nome = nome;
 		this.tipo = tipo;
+		this.descricao = descricao;
+		this.urlFoto = urlFoto;
+		this.urlVideo = urlVideo;
+		this.latitude = latitude;
+		this.longitude = longitude;
 	}
+
 	public Long getId() {
 		return id;
 	}
@@ -42,8 +55,45 @@ public class Carro {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
-	
-	
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
+
+	public String getUrlVideo() {
+		return urlVideo;
+	}
+
+	public void setUrlVideo(String urlVideo) {
+		this.urlVideo = urlVideo;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
 	
 }
