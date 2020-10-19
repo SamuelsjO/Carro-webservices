711072f02ef07732848734a594304d81487abcf0
Samuel Oliveira <soliveira@cpqd.com.br>
2020-10-18 23:07:59 -0300

    Implementando metodo UserDetails

--- src/main/java/com/samuelTI/api/controllers/IndexController.java
+++ src/main/java/com/samuelTI/api/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.samuelTI.api.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,12 @@ public class IndexController {
 		@GetMapping
 		public String hello() {
 			return "API dos Carros - car-api-webservices";
+		}
+		
+		@GetMapping("/userInfo")
+		public UserDetails userInfo(@AuthenticationPrincipal UserDetails user) {
+			return user;
+			
 		}
 
 }
